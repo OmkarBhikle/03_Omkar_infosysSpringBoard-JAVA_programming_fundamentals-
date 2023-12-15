bea83a8021ac05b1452925e0211cbdebf1bf0c4f
OmkarBhikle <103316347+OmkarBhikle@users.noreply.github.com>
2023-12-15 15:43:50 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,56 +1,37 @@
+package Day4.Exceptions;
+
 public class Employee {
-    public int empId;
-    public String name;
-    public double salary;
+    private String empName;
+    private int empAge;
+    private double empSalary;
 
-    public double getSalary() {
-        return salary;
+    public Employee(String empName, int empAge, double empSalary) {
+        this.empName = empName;
+        this.empAge = empAge;
+        this.empSalary = empSalary;
     }
 
-    public void setSalary(double sal) {
-        salary = sal;
+    public String getEmpName() {
+        return empName;
     }
 
-    public int getEmpId() {
-        return empId;
+    public void setEmpName(String empName) {
+        this.empName = empName;
     }
 
-    public void setEmpId(int emp) {
-        empId = emp;
+    public int getEmpAge() {
+        return empAge;
     }
 
-    public String getName() {
-        return name;
+    public void setEmpAge(int empAge) {
+        this.empAge = empAge;
     }
 
-    public void setName(String naam) {
-        name = naam;
+    public double getEmpSalary() {
+        return empSalary;
     }
 
-    public void Display(String naam, double salary) {
-        System.out.println("Name: " + naam);
-        System.out.println("Salary: " + salary);
-    }
-
-    public static void main(String[] args) {
-        PermanentEmployee employee1 = new PermanentEmployee();
-        ContractEmployee employee2 = new ContractEmployee();
-
-        employee1.setName("Anil");
-        employee1.setEmpId(101);
-        employee1.setBasicPay(10000);
-        employee1.setHra(1500);
-        employee1.setExperience(3);
-        employee1.calculateSalary(employee1.getBasicPay(), employee1.getHra(), employee1.getExperience());
-        employee1.setSalary(employee1.getSalary());
-        employee1.Display(employee1.getName(), employee1.getSalary());
-
-        employee2.setName("Ankit");
-        employee2.setEmpId(102);
-        employee2.setWages(500);
-        employee2.setHours(10);
-        employee2.calculateSalary(employee2.getWages(), employee2.getHours());
-        employee2.setSalary(employee2.getSalary());
-        employee2.Display(employee2.getName(), employee2.getSalary());
+    public void setEmpSalary(double empSalary) {
+        this.empSalary = empSalary;
     }
 }
